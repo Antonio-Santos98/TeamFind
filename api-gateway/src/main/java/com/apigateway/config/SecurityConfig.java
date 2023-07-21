67c8d2319eaf3867813a99d6e7e942efa8db75fa
Antonio.Santos98 <antonio.santos@astontech.com>
2023-07-21 12:43:45 -0500

    Fixed the CORS issue with adding to the Security config file in API-Gateway

--- api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
@@ -5,21 +5,28 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.List;
 
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity){
-        serverHttpSecurity.csrf()
-                .disable()
-                .authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http.cors().configurationSource(request -> {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(List.of("*"));
+            configuration.setAllowedMethods(List.of("*"));
+            configuration.setAllowedHeaders(List.of("*"));
+            return configuration;
+        });
+        http.csrf().disable().authorizeExchange(exchange -> exchange.pathMatchers("/eureka/**")
                         .permitAll()
                         .anyExchange()
                         .authenticated())
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
-        return serverHttpSecurity.build();
+        return http.build();
     }
 }
