b484038b73c8cceaeade3eb9b5e46aff8987af10
Antonio.Santos98 <antonio.santos@astontech.com>
2023-08-04 17:12:05 -0500

    Fixed issues with not being able to create a user but now having issues with logging in and returning a token

--- api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/apigateway/config/SecurityConfig.java
@@ -1,9 +1,18 @@
 package com.apigateway.config;
 
+import lombok.RequiredArgsConstructor;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakLogoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 
@@ -12,7 +21,15 @@ import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+//    private final KeycloakLogoutHandler keycloakLogoutHandler;
+//
+//    @Bean
+//    protected SessionAuthenticationStrategy sessionAuthenticationStrategy(){
+//        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+//    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
         http.cors().configurationSource(request -> {
@@ -30,4 +47,20 @@ public class SecurityConfig {
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/api/**")
+//                .hasRole("USER")
+//                .anyRequest()
+//                .permitAll();
+//        http.oauth2Login()
+//                .and()
+//                .logout()
+//                .addLogoutHandler(keycloakLogoutHandler)
+//                .logoutSuccessUrl("/");
+//        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+//        return http.build();
+//    }
 }
--- api-gateway/src/main/java/com/apigateway/service/UserService.java
+++ api-gateway/src/main/java/com/apigateway/service/UserService.java
@@ -6,8 +6,26 @@ import com.apigateway.exceptions.UserNotFound;
 import com.apigateway.model.User;
 import com.apigateway.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.representations.AccessTokenResponse;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
+
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.keycloak.representations.idm.UserRepresentation;
+import org.keycloak.representations.idm.CredentialRepresentation;
+
+import javax.ws.rs.core.Response;
 
 @Service
 @RequiredArgsConstructor
@@ -23,7 +41,8 @@ public class UserService {
             if(!encoder.matches(user.getPassword(), attemptUser.getPassword())){
                 throw new IncorrectPasswordException();
             }else{
-                return "Successfully Logged In!";
+                String jwtToken = obtainJwtToken(user);
+                return jwtToken;
             }
         }else{
             throw new UserNotFound(user.getUserName());
@@ -35,10 +54,90 @@ public class UserService {
             User newUser = new User();
             newUser.setUserName(user.getUserName());
             newUser.setPassword(encoder.encode(user.getPassword()));
+            createUserInKeycloak(newUser);
+
             userRepo.save(newUser);
+
             return "You have been registered successfully!";
         }else{
             throw new UserExists(user.getUserName());
         }
     }
+
+    public String obtainJwtToken(User user){
+        String url = "http://keycloak:8080/auth/realms/teamfinder-realm/protocol/openid-connect/token";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "password");
+        map.add("client_id", "spring-cloud-client");
+        map.add("username", user.getUserName());
+        map.add("password", user.getPassword());
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
+
+        // Extract the access_token from the response
+        String jwtToken = new JSONObject(response.getBody()).getString("access_token");
+        return jwtToken;
+    }
+
+    private void createUserInKeycloak(User user) {
+        String serverUrl = "http://keycloak:8080/auth";
+        String realm = "teamfinder-realm";
+        String clientId = "spring-cloud-client";
+        String clientSecret = "Zumxxlmnc0SNmHq8Md1TY9Z6XlPQSAgl";
+        String adminUsername = "admin";
+        String adminPassword = "admin";
+
+        // Step 1: Obtain an Access Token
+        Keycloak keycloak = KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.PASSWORD)
+                .clientId(clientId)
+                .username(adminUsername)
+                .password(adminPassword)
+                .clientSecret(clientSecret)
+                .build();
+
+        AccessTokenResponse tokenResponse = keycloak.tokenManager().getAccessToken();
+
+
+        // Use the access token obtained from the response
+        String accessToken = tokenResponse.getToken();
+
+        // Step 2: Use the Access Token in API Requests
+        Keycloak adminClient = KeycloakBuilder.builder()
+                .serverUrl(serverUrl)
+                .realm(realm)
+                .grantType(OAuth2Constants.CLIENT_CREDENTIALS)
+                .clientId(clientId)
+                .clientSecret(clientSecret)
+                .authorization(accessToken) // Set the access token as the authorization header
+                .build();
+
+        UserRepresentation userRepresentation = new UserRepresentation();
+        userRepresentation.setUsername(user.getUserName());
+        userRepresentation.setEnabled(true);
+
+        // Creating a new Keycloak user
+        Response result = adminClient.realm(realm).users().create(userRepresentation);
+
+        if (result.getStatus() == 201) {
+            String location = result.getLocation().toString();
+            String userId = location.substring(location.lastIndexOf("/") + 1);
+
+            // Set password
+            CredentialRepresentation passwordCred = new CredentialRepresentation();
+            passwordCred.setTemporary(false);
+            passwordCred.setType(CredentialRepresentation.PASSWORD);
+            passwordCred.setValue(user.getPassword());
+
+            adminClient.realm(realm).users().get(userId).resetPassword(passwordCred);
+        }
+    }
+
 }
